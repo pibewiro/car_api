328884416e11b38178238f6b7e1077c738cdb819
prince <princeibewiro@indecex.com.br>
2020-04-20 00:00:28 -0300

    upd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./config/server');
 require('dotenv').config()
 
-const port = process.env.PORT;
+const port = process.env.PORT || 9000;
 
 app.listen(port, () => console.log(`Connected to Port ${port}`))
